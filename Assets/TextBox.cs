8238901d52492ba0e26e59fdfb54b169e6e182e5
raa0121 <raa0121@gmail.com>
2014-09-21 00:57:35 +0900

    fix textField

--- Assets/TextBox.cs
+++ Assets/TextBox.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class TextBox : MonoBehaviour {
 
+	private string text = "";
 	// Use this for initialization
 	void Start () {
 	
@@ -12,10 +13,10 @@ public class TextBox : MonoBehaviour {
 	void Update () {
 	
 	}
-	void onGUI () {
+	void OnGUI () {
 		Rect rect1 = new Rect(10, Screen.height-30, Screen.width-60, 30);
 		Rect rect2 = new Rect(Screen.width-60, Screen.height-30, 60, 30); 
-		GUI.TextField (rect1, "", 30);
+		text = GUI.TextField (rect1, text, 30);
 		GUI.Button (rect2, "post");
 	}
 }
