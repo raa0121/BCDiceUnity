c54b67eebde8d83ca262595a4a767f2800dfdeef
raa0121 <raa0121@gmail.com>
2014-09-21 00:42:06 +0900

    init

---
+++ Assets/TextBox.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextBox : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void onGUI () {
+		Rect rect1 = new Rect(10, Screen.height-30, Screen.width-60, 30);
+		Rect rect2 = new Rect(Screen.width-60, Screen.height-30, 60, 30); 
+		GUI.TextField (rect1, "", 30);
+		GUI.Button (rect2, "post");
+	}
+}
